89308e587cdd657f19962180f6763ef86a1fafd6
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2022-07-03 14:29:52 -0300

    add category model

--- application/src/main/java/com/vinigracindo/movies/application/UseCase.java
+++ application/src/main/java/com/vinigracindo/movies/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.vinigracindo.movies.application;
 
-import com.vinigracindo.movies.domain.Category;
-
 public class UseCase {
     public Category execute() {
         return new Category();
--- domain/src/main/java/com/vinigracindo/movies/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package com.vinigracindo.movies.domain;
-
-public class Category {
-    public String id;
-}
